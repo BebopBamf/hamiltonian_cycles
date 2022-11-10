ed8c9a7405982101bb36b6a8759e4a1cf8687d19
Euan Mendoza <bebopbamf@effectfree.dev>
2022-11-10 11:50:22 +1100

    updated code for cli support

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hamiltonian Cycles
+
+## Running NIM Project
+
+## Building Document
--- code/src/main.nim
+++ code/src/main.nim
@@ -1,5 +1,9 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import std/parseopt
+
+proc write_help() =
+  echo("Hamiltonian Cycle Algorithms CLI")
+  echo("V 0.0.1")
 
 when isMainModule:
-  echo("Hello, World!")
+  echo("Starting Algorithms!")
+  write_help()
